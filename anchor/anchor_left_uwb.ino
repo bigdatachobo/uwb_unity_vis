230dd6c14abcc3daa690a06016efc538968b2887
bigdatachobo <61238033+bigdatachobo@users.noreply.github.com>
2023-05-31 10:15:22 +0900

    Update anchor_left_uwb.ino

--- anchor/anchor_left_uwb.ino
+++ anchor/anchor_left_uwb.ino
@@ -8,11 +8,17 @@
 
 #include <SPI.h>
 #include "DW1000Ranging.h"
+#include "DW1000.h"
+
+#define SPI_SCK 18
+#define SPI_MISO 19
+#define SPI_MOSI 23
+#define DW_CS 4
 
 // connection pins
-const uint8_t PIN_SCK = 18;
-const uint8_t PIN_MOSI = 23;
-const uint8_t PIN_MISO = 19;
+//const uint8_t PIN_SCK = 18;
+//const uint8_t PIN_MOSI = 23;
+//const uint8_t PIN_MISO = 19;
 
 // original pin num
 //const uint8_t PIN_SS = 15;
@@ -20,15 +26,15 @@ const uint8_t PIN_MISO = 19;
 //const uint8_t PIN_IRQ = 22;
 
 // dwm1000 esp32 integrated board pin num
-const uint8_t PIN_SS = 4;
-const uint8_t PIN_RST = 27;
-const uint8_t PIN_IRQ = 34;
+const uint8_t PIN_RST = 27; // reset pin
+const uint8_t PIN_IRQ = 34; // irq pin
+const uint8_t PIN_SS = 4;   // spi select pin
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
   //init the configuration
-  SPI.begin(PIN_SCK, PIN_MISO, PIN_MOSI);
+  SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
   DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ);  //Reset, CS, IRQ pin
   //define the sketch as anchor. It will be great to dynamically change the type of module
   DW1000Ranging.attachNewRange(newRange);
@@ -55,6 +61,16 @@ void loop() {
 }
 
 void newRange() {
+  
+  Serial.print("from: ");
+  Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
+  Serial.print("\t Range: ");
+  Serial.print(DW1000Ranging.getDistantDevice()->getRange());
+  Serial.print(" m");
+  Serial.print("\t RX power: ");
+  Serial.print(DW1000Ranging.getDistantDevice()->getRXPower());
+  Serial.println(" dBm");
+  
   updateRange(DW1000Ranging.getDistantDevice()->getRange());
 }
 
@@ -62,6 +78,8 @@ void newBlink(DW1000Device* device) {
 }
 
 void inactiveDevice(DW1000Device* device) {
+  Serial.print("delete inactive device: ");
+  Serial.println(device->getShortAddress(), HEX);
 }
 
 void updateRange(float range) {
